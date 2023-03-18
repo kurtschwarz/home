d96ecce31321d98f19a19b775259d5eaf8ba8ce1
Kurt Schwarz <kurt@unkn.co>
2023-03-18 15:28:08 +0000

    Adds ability to provision certs with cert-manager

--- system/cert-manager/main.go
+++ system/cert-manager/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	kubernetes "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
 	apiextensions "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apiextensions"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
@@ -11,11 +14,13 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		config := config.New(ctx, "certManager")
-		_, err := apiextensions.NewCustomResource(ctx, "cert-manager-cluster-issuer", &apiextensions.CustomResourceArgs{
+
+		clusterIssuerName := "cert-manager-lets-encrypt-issuer"
+		clusterIssuer, err := apiextensions.NewCustomResource(ctx, clusterIssuerName, &apiextensions.CustomResourceArgs{
 			ApiVersion: pulumi.String("cert-manager.io/v1"),
 			Kind:       pulumi.String("ClusterIssuer"),
 			Metadata: &metav1.ObjectMetaArgs{
-				Name: pulumi.String("letsencrypt"),
+				Name: pulumi.String(clusterIssuerName),
 			},
 			OtherFields: kubernetes.UntypedArgs{
 				"spec": kubernetes.UntypedArgs{
@@ -38,6 +43,38 @@ func main() {
 				},
 			},
 		})
+
+		domains := []string{}
+		config.RequireObject("domains", &domains)
+
+		for _, domain := range domains {
+			domainName := strings.Replace(domain, ".", "-", -1)
+			certificateName := fmt.Sprintf("cert-manager-cert-%s", domainName)
+			_, err := apiextensions.NewCustomResource(ctx, certificateName, &apiextensions.CustomResourceArgs{
+				ApiVersion: pulumi.String("cert-manager.io/v1"),
+				Kind:       pulumi.String("Certificate"),
+				Metadata: &metav1.ObjectMetaArgs{
+					Name: pulumi.String(certificateName),
+				},
+				OtherFields: kubernetes.UntypedArgs{
+					"spec": kubernetes.UntypedArgs{
+						"commonName": pulumi.String(domain),
+						"secretName": pulumi.String(domainName),
+						"dnsNames": pulumi.StringArray{
+							pulumi.String(domain),
+						},
+						"issuerRef": kubernetes.UntypedArgs{
+							"name": clusterIssuer.Metadata.Name().Elem(),
+							"kind": "ClusterIssuer",
+						},
+					},
+				},
+			})
+
+			if err != nil {
+				return err
+			}
+		}
 
 		return err
 	})
