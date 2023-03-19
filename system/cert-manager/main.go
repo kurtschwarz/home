906ae8904703f93bdf48c1fc6be8f81fae993bc8
Kurt Schwarz <kurt@unkn.co>
2023-03-19 00:39:13 +0000

    Fixes grafana ssl not working due to namespace change

--- system/cert-manager/main.go
+++ system/cert-manager/main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"strings"
-
 	kubernetes "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
 	apiextensions "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apiextensions"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
@@ -15,18 +12,17 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		config := config.New(ctx, "certManager")
 
-		clusterIssuerName := "cert-manager-lets-encrypt-issuer"
-		clusterIssuer, err := apiextensions.NewCustomResource(ctx, clusterIssuerName, &apiextensions.CustomResourceArgs{
+		_, err := apiextensions.NewCustomResource(ctx, "cert-manager-lets-encrypt-issuer", &apiextensions.CustomResourceArgs{
 			ApiVersion: pulumi.String("cert-manager.io/v1"),
 			Kind:       pulumi.String("ClusterIssuer"),
 			Metadata: &metav1.ObjectMetaArgs{
-				Name: pulumi.String(clusterIssuerName),
+				Name: pulumi.String("cert-manager-lets-encrypt-issuer"),
 			},
 			OtherFields: kubernetes.UntypedArgs{
 				"spec": kubernetes.UntypedArgs{
 					"acme": kubernetes.UntypedArgs{
 						"email":  pulumi.String(config.Require("acmeEmail")),
-						"server": pulumi.String("https://acme-staging-v02.api.letsencrypt.org/directory"),
+						"server": pulumi.String("https://acme-v02.api.letsencrypt.org/directory"),
 						"privateKeySecretRef": kubernetes.UntypedArgs{
 							"name": pulumi.String("letsencrypt-account-key"),
 						},
@@ -43,38 +39,6 @@ func main() {
 				},
 			},
 		})
-
-		domains := []string{}
-		config.RequireObject("domains", &domains)
-
-		for _, domain := range domains {
-			domainName := strings.Replace(domain, ".", "-", -1)
-			certificateName := fmt.Sprintf("cert-manager-cert-%s", domainName)
-			_, err := apiextensions.NewCustomResource(ctx, certificateName, &apiextensions.CustomResourceArgs{
-				ApiVersion: pulumi.String("cert-manager.io/v1"),
-				Kind:       pulumi.String("Certificate"),
-				Metadata: &metav1.ObjectMetaArgs{
-					Name: pulumi.String(certificateName),
-				},
-				OtherFields: kubernetes.UntypedArgs{
-					"spec": kubernetes.UntypedArgs{
-						"commonName": pulumi.String(domain),
-						"secretName": pulumi.String(domainName),
-						"dnsNames": pulumi.StringArray{
-							pulumi.String(domain),
-						},
-						"issuerRef": kubernetes.UntypedArgs{
-							"name": clusterIssuer.Metadata.Name().Elem(),
-							"kind": "ClusterIssuer",
-						},
-					},
-				},
-			})
-
-			if err != nil {
-				return err
-			}
-		}
 
 		return err
 	})
--- system/grafana/main.go
+++ system/grafana/main.go
@@ -12,6 +12,73 @@ import (
 	config "github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
+func provisionVolumes(ctx *pulumi.Context, namespace pulumi.StringInput) (*corev1.PersistentVolume, *corev1.PersistentVolumeClaim, error) {
+	persistentVolume, err := corev1.NewPersistentVolume(
+		ctx,
+		"grafana-pv",
+		&corev1.PersistentVolumeArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String("grafana-pv"),
+				Labels: pulumi.StringMap{
+					"app":  pulumi.String("grafana"),
+					"type": pulumi.String("local"),
+				},
+				Namespace: namespace,
+			},
+			Spec: &corev1.PersistentVolumeSpecArgs{
+				StorageClassName: pulumi.String("microk8s-hostpath"),
+				Capacity: pulumi.StringMap{
+					"storage": pulumi.String("20Gi"),
+				},
+				AccessModes: &pulumi.StringArray{
+					pulumi.String("ReadWriteOnce"),
+				},
+				HostPath: &corev1.HostPathVolumeSourceArgs{
+					Path: pulumi.String("/mnt/appdata/grafana/data"),
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	persistentVolumeClaim, err := corev1.NewPersistentVolumeClaim(
+		ctx,
+		"grafana-pv-claim",
+		&corev1.PersistentVolumeClaimArgs{
+			Metadata: &metav1.ObjectMetaArgs{
+				Name: pulumi.String("grafana-pv-claim"),
+				Labels: pulumi.StringMap{
+					"app": pulumi.String("grafana"),
+				},
+				Namespace: namespace,
+			},
+			Spec: &corev1.PersistentVolumeClaimSpecArgs{
+				StorageClassName: pulumi.String("microk8s-hostpath"),
+				Resources: &corev1.ResourceRequirementsArgs{
+					Requests: pulumi.StringMap{
+						"storage": pulumi.String("20Gi"),
+					},
+				},
+				AccessModes: &pulumi.StringArray{
+					pulumi.String("ReadWriteOnce"),
+				},
+			},
+		},
+		pulumi.DependsOn([]pulumi.Resource{
+			persistentVolume,
+		}),
+	)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return persistentVolume, persistentVolumeClaim, nil
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		config := config.New(ctx, "grafana")
@@ -27,6 +94,44 @@ func main() {
 		}
 
 		namespace := namespaceRef.GetOutput(pulumi.String("namespace")).AsStringOutput()
+
+		persistentVolume, persistentVolumeClaim, err := provisionVolumes(
+			ctx,
+			namespace,
+		)
+
+		if err != nil {
+			return err
+		}
+
+		certificate, err := apiextensions.NewCustomResource(ctx, "grafana-kurtina-ca-cert", &apiextensions.CustomResourceArgs{
+			ApiVersion: pulumi.String("cert-manager.io/v1"),
+			Kind:       pulumi.String("Certificate"),
+			Metadata: &metav1.ObjectMetaArgs{
+				Name:      pulumi.String("grafana-kurtina-ca-cert"),
+				Namespace: namespace,
+			},
+			OtherFields: kubernetes.UntypedArgs{
+				"spec": kubernetes.UntypedArgs{
+					"commonName": pulumi.String("grafana.kurtina.ca"),
+					"secretName": pulumi.String("grafana-kurtina-ca-cert"),
+					"secretTemplate": kubernetes.UntypedArgs{
+						"namespace": namespace,
+					},
+					"dnsNames": pulumi.StringArray{
+						pulumi.String("grafana.kurtina.ca"),
+					},
+					"issuerRef": kubernetes.UntypedArgs{
+						"name": pulumi.String("cert-manager-lets-encrypt-issuer"),
+						"kind": pulumi.String("ClusterIssuer"),
+					},
+				},
+			},
+		})
+
+		if err != nil {
+			return err
+		}
 
 		deploymentLabels := pulumi.StringMap{"app": pulumi.String("grafana")}
 		deployment, err := appsv1.NewDeployment(
@@ -65,12 +170,29 @@ func main() {
 											Protocol:      pulumi.String("TCP"),
 										},
 									},
+									VolumeMounts: &corev1.VolumeMountArray{
+										&corev1.VolumeMountArgs{
+											Name:      pulumi.String("grafana-pv"),
+											MountPath: pulumi.String("/var/lib/grafana"),
+										},
+									},
+								},
+							},
+							Volumes: &corev1.VolumeArray{
+								&corev1.VolumeArgs{
+									Name: pulumi.String("grafana-pv"),
+									PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSourceArgs{
+										ClaimName: persistentVolumeClaim.Metadata.Name().Elem(),
+									},
 								},
 							},
 						},
 					},
 				},
 			},
+			pulumi.DependsOn([]pulumi.Resource{
+				persistentVolume,
+			}),
 		)
 
 		if err != nil {
@@ -136,12 +258,15 @@ func main() {
 							},
 						},
 						"tls": kubernetes.UntypedArgs{
-							"secretName": pulumi.String("grafana-kurtina-ca"),
+							"secretName": pulumi.String("grafana-kurtina-ca-cert"),
 						},
 					},
 				},
 			},
 			pulumi.Parent(service),
+			pulumi.DependsOn([]pulumi.Resource{
+				certificate,
+			}),
 		)
 
 		if err != nil {
