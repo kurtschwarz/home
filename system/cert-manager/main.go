e503bf7b20e56182945612c7740fd41ab021936a
Kurt Schwarz <kurt@unkn.co>
2023-03-28 00:18:56 +0000

    Adjusts cert-manager deployment to provision the full manifest

--- system/cert-manager/main.go
+++ system/cert-manager/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	kubernetes "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
 	apiextensions "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apiextensions"
 	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
+	yaml "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/yaml"
 	pulumi "github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	config "github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
@@ -12,29 +15,45 @@ func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		config := config.New(ctx, "certManager")
 
-		_, err := apiextensions.NewCustomResource(ctx, "cert-manager-lets-encrypt-issuer", &apiextensions.CustomResourceArgs{
-			ApiVersion: pulumi.String("cert-manager.io/v1"),
-			Kind:       pulumi.String("ClusterIssuer"),
-			Metadata: &metav1.ObjectMetaArgs{
-				Name: pulumi.String("cert-manager-lets-encrypt-issuer"),
+		manifest, err := yaml.NewConfigFile(
+			ctx,
+			"cert-manager-manifest",
+			&yaml.ConfigFileArgs{
+				File: fmt.Sprintf("https://github.com/cert-manager/cert-manager/releases/download/%s/cert-manager.yaml", config.Require("version")),
 			},
-			OtherFields: kubernetes.UntypedArgs{
-				"spec": kubernetes.UntypedArgs{
-					"acme": kubernetes.UntypedArgs{
-						"email":  pulumi.String(config.Require("acmeEmail")),
-						"server": pulumi.String("https://acme-v02.api.letsencrypt.org/directory"),
-						"privateKeySecretRef": kubernetes.UntypedArgs{
-							"name": pulumi.String("letsencrypt-account-key"),
-						},
-						"solvers": []kubernetes.UntypedArgs{
-							{
-								"http01": kubernetes.UntypedArgs{
-									"ingress": kubernetes.UntypedArgs{
-										"class": "traefik-cert-manager",
-										"ingressTemplate": kubernetes.UntypedArgs{
-											"metadata": kubernetes.UntypedArgs{
-												"annotations": kubernetes.UntypedArgs{
-													"traefik.ingress.kubernetes.io/router.priority": "100",
+		)
+
+		if err != nil {
+			return err
+		}
+
+		_, err = apiextensions.NewCustomResource(
+			ctx,
+			"cert-manager-lets-encrypt-issuer",
+			&apiextensions.CustomResourceArgs{
+				ApiVersion: pulumi.String("cert-manager.io/v1"),
+				Kind:       pulumi.String("ClusterIssuer"),
+				Metadata: &metav1.ObjectMetaArgs{
+					Name: pulumi.String("cert-manager-lets-encrypt-issuer"),
+				},
+				OtherFields: kubernetes.UntypedArgs{
+					"spec": kubernetes.UntypedArgs{
+						"acme": kubernetes.UntypedArgs{
+							"email":  pulumi.String(config.Require("acmeEmail")),
+							"server": pulumi.String("https://acme-v02.api.letsencrypt.org/directory"),
+							"privateKeySecretRef": kubernetes.UntypedArgs{
+								"name": pulumi.String("letsencrypt-account-key"),
+							},
+							"solvers": []kubernetes.UntypedArgs{
+								{
+									"http01": kubernetes.UntypedArgs{
+										"ingress": kubernetes.UntypedArgs{
+											"class": "traefik-cert-manager",
+											"ingressTemplate": kubernetes.UntypedArgs{
+												"metadata": kubernetes.UntypedArgs{
+													"annotations": kubernetes.UntypedArgs{
+														"traefik.ingress.kubernetes.io/router.priority": "100",
+													},
 												},
 											},
 										},
@@ -45,7 +64,8 @@ func main() {
 					},
 				},
 			},
-		})
+			pulumi.Parent(manifest),
+		)
 
 		return err
 	})
