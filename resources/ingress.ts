d1fa0230dcbf3ddf98c9c02e6f55fdce319ea153
Kurt Schwarz <kurt@unkn.co>
2024-02-04 21:49:21 +0000

    feat: adds tcp/udp ingress types

--- resources/ingress.ts
+++ resources/ingress.ts
@@ -5,6 +5,8 @@ import { Certificate } from './certificate'
 
 export enum IngressType {
   HTTP = 'http',
+  TCP = 'tcp',
+  UDP = 'udp',
 }
 
 export enum AccessType {
@@ -14,16 +16,19 @@ export enum AccessType {
 
 type IngressArgs = {
   type: IngressType
-  access: AccessType
-  domain: pulumi.Input<string>
+  access?: AccessType
+  domain?: pulumi.Input<string>
   namespace: pulumi.Input<string>
   serviceName: pulumi.Input<string>
   servicePort: pulumi.Input<number>
+  entryPoints?: pulumi.Input<string[]>
 }
 
 export class Ingress extends pulumi.ComponentResource {
   private types: Map<IngressType, () => void> = new Map([
     [IngressType.HTTP, this._httpIngressResources],
+    [IngressType.TCP, this._tcpIngressResources],
+    [IngressType.UDP, this._udpIngressResources],
   ])
 
   private certificate?: Certificate
@@ -39,6 +44,10 @@ export class Ingress extends pulumi.ComponentResource {
   }
 
   private _httpIngressResources(): void {
+    if (!this.args.domain) {
+      return
+    }
+
     this.certificate = new Certificate(
       this.name,
       {
@@ -83,6 +92,70 @@ export class Ingress extends pulumi.ComponentResource {
         dependsOn: [
           this.certificate,
         ],
+      },
+    )
+  }
+
+  private _tcpIngressResources(): void {
+    new kube.apiextensions.CustomResource(
+      `${this.name}-tcp-ingress-route`,
+      {
+        apiVersion: 'traefik.containo.us/v1alpha1',
+        kind: 'IngressRouteTCP',
+        metadata: {
+          name: `${this.name}-tcp`,
+          namespace: this.args.namespace,
+        },
+        spec: {
+          entryPoints: this.args.entryPoints,
+          routes: [
+            {
+              match: `HostSNI(\`*\`)`,
+              kind: 'Rule',
+              services: [
+                {
+                  name: this.args.serviceName,
+                  port: this.args.servicePort,
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        parent: this
+      },
+    )
+  }
+
+  private _udpIngressResources(): void {
+    new kube.apiextensions.CustomResource(
+      `${this.name}-udp-ingress-route`,
+      {
+        apiVersion: 'traefik.containo.us/v1alpha1',
+        kind: 'IngressRouteUDP',
+        metadata: {
+          name: `${this.name}-udp`,
+          namespace: this.args.namespace,
+        },
+        spec: {
+          entryPoints: this.args.entryPoints,
+          routes: [
+            {
+              match: `HostSNI(\`*\`)`,
+              kind: 'Rule',
+              services: [
+                {
+                  name: this.args.serviceName,
+                  port: this.args.servicePort,
+                },
+              ],
+            },
+          ],
+        },
+      },
+      {
+        parent: this
       },
     )
   }
